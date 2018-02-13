ee7cbd4e1df0a11201d518511e48a57c73ee51d6
Ou Changhua <ochinchina@gmail.com>
2018-02-13 09:20:45 +0000

    support yaml configuration file

--- kube-tmpl.py
+++ kube-tmpl.py
@@ -6,18 +6,42 @@ import json
 import argparse
 import os
 import tempfile
+import yaml
 
 def load_configs( config_files ):
+    """
+    load the .json or .yaml configuration file. if same item in multiple
+    configuration file, the later one will overwrite the previous one
+
+    Args:
+        config_files: list of configuration file, and eash one configuration file
+        must be in json or in yaml format
+    Returns:
+        the merged configuration items
+    """
     result = {}
     for config_file in config_files:
         try:
             with open( config_file ) as fp:
-                result.update( json.load(fp) )
+                if config_file.endswith( ".json" ) or config_file.endswith( ".js" ):
+                    result.update( json.load(fp) )
+                else:
+                    result.update( yaml.safe_load( fp ) )
         except:
             pass
     return result
 
-def change_project( args, action ):
+def change_deployment( args, action ):
+    """
+    change the kubernetes deployment
+
+    Args:
+        args: the command line arguments
+        action: must be create or delete
+    Returns:
+        if --dry-run flag is in the command line, only print the changed template
+        otherwise it will call kubectl command to create/delete deployments
+    """
     templateLoader = jinja2.FileSystemLoader( searchpath = args.searchpath )
     templateEnv = jinja2.Environment( loader=templateLoader )
     template = templateEnv.get_template( args.template )
@@ -42,13 +66,13 @@ def parse_args():
     install_parser.add_argument( "--template", help = "the kubernetes .yaml template file", required = True )
     install_parser.add_argument( "--config-file", help = "the configuration files", nargs = "*", required = True )
     install_parser.add_argument( "--dry-run", help = "run without install", action = "store_true" )
-    install_parser.set_defaults( func = functools.partial( change_project, action = "create" ) )
+    install_parser.set_defaults( func = functools.partial( change_deployment, action = "create" ) )
     delete_parser = subparsers.add_parser( "delete", help = "delete a project" )
     delete_parser.add_argument( "--searchpath", help = "the search path of kubernetes .yaml file", required = False, default = "./" )
     delete_parser.add_argument( "--template", help = "the kubernetes .yaml template file", required = True )
     delete_parser.add_argument( "--config-file", help = "the configuration files", nargs = "*", required = True )
     delete_parser.add_argument( "--dry-run", help = "run without install", action = "store_true" )
-    delete_parser.set_defaults( func = functools.partial( change_project, action = "delete" ) )
+    delete_parser.set_defaults( func = functools.partial( change_deployment, action = "delete" ) )
     return parser.parse_args()
 
 def main():
