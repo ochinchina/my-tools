33bb6ece603b48cf62f10724839be6528ef40649
Ou Changhua <ochinchina@gmail.com>
2018-02-14 07:50:18 +0000

    set the values from the command line

--- kube-tmpl.py
+++ kube-tmpl.py
@@ -8,27 +8,82 @@ import os
 import tempfile
 import yaml
 
-def load_configs( config_files ):
+class NameItem:
+    def __init__( self, name ):
+        self.name = name
+        self.index = -1
+        if name.endswith( ']' ):
+            pos = name.rfind('[' )
+            if pos != -1:
+                self.index = int ( name[ pos + 1: -1 ].strip() )
+                self.name = name[0:pos]
+
+    def is_array( self ):
+        return self.index >= 0
+
+                
+def load_value_files( value_files ):
     """
     load the .json or .yaml configuration file. if same item in multiple
     configuration file, the later one will overwrite the previous one
 
     Args:
-        config_files: list of configuration file, and eash one configuration file
+        value_files: list of configuration file, and eash one configuration file
         must be in json or in yaml format
     Returns:
         the merged configuration items
     """
     result = {}
-    for config_file in config_files:
+    for value_file in value_files:
         try:
-            with open( config_file ) as fp:
-                if config_file.endswith( ".json" ) or config_file.endswith( ".js" ):
+            with open( value_file ) as fp:
+                if value_file.endswith( ".json" ) or value_file.endswith( ".js" ):
                     result.update( json.load(fp) )
                 else:
                     result.update( yaml.safe_load( fp ) )
         except:
             pass
+    return result
+
+def parse_values( values ):
+    """
+    parse the values from command line
+
+    Args:
+        values: a list of value, each value will be in following format:
+          name1.name2.name3=value
+          or name1[0].name2.name3= value
+    Returns:
+        a dictionary
+    """
+    result = {}
+    for value in values:
+        pos = value.find( '=' )
+        if pos == -1:
+           pos = value.find( ':' )
+        if pos != -1:
+           key = value[0:pos].strip()
+           v = value[pos+1:].strip()
+           words = key.split( ".")
+           items = []
+           for w in words:
+               items.append( NameItem( w ) )
+           cur_result = result
+           for i, item in enumerate( items ):
+               if item.is_array():
+                   if item.name not in cur_result:
+                       cur_result[ item.name ] = []
+                   while len( cur_result[ item.name ] ) <= item.index:
+                       cur_result[ item.name ].append( {} )
+                   if i == len( items ) -1:
+                       cur_result[ item.name ][item.index] = v
+                   else:
+                       cur_result = cur_result[ item.name ][item.index]
+               else:
+                   if item.name not in cur_result:
+                       cur_result[ item.name ] = {}
+                   if i == len( items ) -1:
+                       cur_result[ item.name ] = v
     return result
 
 def change_deployment( args, action ):
@@ -45,7 +100,11 @@ def change_deployment( args, action ):
     templateLoader = jinja2.FileSystemLoader( searchpath = args.searchpath )
     templateEnv = jinja2.Environment( loader=templateLoader )
     template = templateEnv.get_template( args.template )
-    config = load_configs( args.config_file )
+    config = {}
+    if args.value_files:
+        config.update( load_value_files( args.value_files) )
+    if args.values:
+        config.update( parse_values( args.values ) )
     if args.dry_run:
         print template.render( config )
     else:
@@ -60,23 +119,22 @@ def change_deployment( args, action ):
 
 def parse_args():
     parser = argparse.ArgumentParser( description = "generate template" )
+    parser.add_argument( "--searchpath", help = "the search path of kubernetes .yaml file, default is current directory", required = False, default = "./" )
+    parser.add_argument( "--template", help = "the kubernetes .yaml template file", required = True )
+    parser.add_argument( "--value-files", help = "the configuration files", nargs = "*", required = False )
+    parser.add_argument( "--dry-run", help = "run without real action", action = "store_true" )
+    parser.add_argument( "--values", help = "the values", nargs = "*", required = False )
     subparsers = parser.add_subparsers( help = "install a project" )
     install_parser = subparsers.add_parser( "install", help = "install a project" )
-    install_parser.add_argument( "--searchpath", help = "the search path of kubernetes .yaml file", required = False, default = "./" )
-    install_parser.add_argument( "--template", help = "the kubernetes .yaml template file", required = True )
-    install_parser.add_argument( "--config-file", help = "the configuration files", nargs = "*", required = True )
-    install_parser.add_argument( "--dry-run", help = "run without install", action = "store_true" )
     install_parser.set_defaults( func = functools.partial( change_deployment, action = "create" ) )
     delete_parser = subparsers.add_parser( "delete", help = "delete a project" )
-    delete_parser.add_argument( "--searchpath", help = "the search path of kubernetes .yaml file", required = False, default = "./" )
-    delete_parser.add_argument( "--template", help = "the kubernetes .yaml template file", required = True )
-    delete_parser.add_argument( "--config-file", help = "the configuration files", nargs = "*", required = True )
-    delete_parser.add_argument( "--dry-run", help = "run without install", action = "store_true" )
     delete_parser.set_defaults( func = functools.partial( change_deployment, action = "delete" ) )
     return parser.parse_args()
 
 def main():
     args = parse_args()
+    print args
+    print args.template
     args.func( args )
 
 main()
