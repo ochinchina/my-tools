b260d895f80400c844e426af68279ee68c3d4e8c
Steven Ou <ochinchina@gmail.com>
2018-10-01 19:37:57 +0800

    python3 support

--- sync-web-time.py
+++ sync-web-time.py
@@ -2,12 +2,18 @@
 
 import os
 import time
-import urllib2
 import sys
 
-r = urllib2.urlopen( "http://baidu.com" if len( sys.argv ) == 1 else sys.argv[1] )
-headers = r.info().dict
+try:
+    import urllib2
+    r = urllib2.urlopen( "http://baidu.com" if len( sys.argv ) == 1 else sys.argv[1] )
+    headers = r.info().dict
+except:
+    import urllib3
+    http = urllib3.PoolManager()
+    r = http.request( "GET", "http://baidu.com" if len( sys.argv ) == 1 else sys.argv[1] )
+    headers = r.getheaders()
+
 if 'date' in headers:
     t = time.strptime( headers['date'], '%a, %d %b %Y %H:%M:%S GMT' )
     os.system( 'date %02d%02d%02d%02d%02d' % ( t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_year ) )
-
